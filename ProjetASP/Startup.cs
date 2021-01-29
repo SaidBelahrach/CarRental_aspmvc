8540fad4958e607b392ec0c485e2b362203892c9
SaidBelahrach <said.belahrach16@gmail.com>
2021-01-29 14:23:31 +0100

    Add project files.

---
+++ ProjetASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetASP.Startup))]
+namespace ProjetASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
