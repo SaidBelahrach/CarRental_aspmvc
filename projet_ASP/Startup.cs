f7b7af6a3939f465cda20aafef4c3e636debfadb
SaidBelahrach <said.belahrach16@gmail.com>
2021-01-29 15:27:14 +0100

    users db is working

---
+++ projet_ASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(projet_ASP.Startup))]
+namespace projet_ASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
