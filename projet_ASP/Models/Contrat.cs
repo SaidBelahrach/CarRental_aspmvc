d1c52652a90a4f977549fb7985fea888ab9c41ea
SaidBelahrach <said.belahrach16@gmail.com>
2021-01-29 21:19:08 +0100

    debug some bugs

--- projet_ASP/Models/Contrat.cs
+++ projet_ASP/Models/Contrat.cs
@@ -9,12 +9,16 @@ namespace projet_ASP.Models
 {
     public class Contrat
     {
-        [Column("ID")]      //ces 3 sont obligatoire
+        [Column("ID")]      //ces 3 sont obligatoires
         [Key]
         [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //auto increment
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  
         public int idContrat { get; set; }
 
+        [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
+        [Display(Name = "Date de location")]
+        public DateTime dateLocation{ get; set; }  
+        
         [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
         [Display(Name = "Date début")]
         public DateTime dateDebut { get; set; }  
@@ -23,9 +27,24 @@ namespace projet_ASP.Models
         [Display(Name = "Date Fin")]
         public DateTime dateFin { get; set; }
 
+        //+ dateCreation
 
         [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
-        [Display(Name = "téléphone")]
-        public string tel { get; set; }
+        [Display(Name = "Type de paiement")]
+        public string typeDePaiement { get; set; }
+
+        [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
+        [Display(Name = "Coût")]
+        public Decimal cout { get; set; }
+
+
+        [ForeignKey("Voiture")]
+        public int matricul { get; set; }
+        Voiture voiture { get; set; }
+
+
+        [ForeignKey("Locataire")]
+        public int idLocataire { get; set; }
+        Locataire locataire { get; set; }
     }
 }
