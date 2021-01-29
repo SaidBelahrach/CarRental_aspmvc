512739a09312a60a0ae453e4c250388ba9a6c5bd
SaidBelahrach <said.belahrach16@gmail.com>
2021-01-29 17:17:06 +0100

    removed app_data from gitignore

---
+++ projet_ASP/Models/Voiture.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace projet_ASP.Models
+{
+    public class Voiture
+    {
+        [Column("ID")]      //ces 3 sont obligatoire
+        [Key]
+        [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
+        [Display(Name = "matricule")]
+        //  [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int matricule { get; set; }
+
+        [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
+        [Display(Name = "marque")]
+        public string marque { get; set; }
+ 
+        
+        [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
+        [Display(Name = "modèl")]
+        public string modèl { get; set; }
+        
+        [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
+        [Display(Name = "couleur")]
+        public string couleur { get; set; } 
+        
+        [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
+        [Display(Name = "coutParJour")]
+        public Decimal coutParJour { get; set; }
+        
+       // [Required(ErrorMessage = "Ce champs est obligatoire", AllowEmptyStrings = false)]
+        [Display(Name = "km")]
+        public Decimal km { get; set; }
+
+
+
+
+    }
+}
+/* + Matricule
++ marque :string
++ annee :int
++ modèl: int
++ marque :DateTime
++ couleur
++ etat
++ km
++ CoutParJour
+*/
