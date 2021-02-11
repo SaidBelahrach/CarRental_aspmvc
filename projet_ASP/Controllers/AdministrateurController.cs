ebe829ae7e9561e4fda44d373dedc30aef79f5c4
EnsafCSharpProj <75618286+EnsafCSharpProj@users.noreply.github.com>
2021-02-11 16:03:55 +0100

    ww

---
+++ projet_ASP/Controllers/AdministrateurController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projet_ASP.Controllers
+{
+    public class AdministrateurController : Controller
+    {
+        // GET: Administrateur
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
