104a6d88eefbf035aececd5a5977a753834511c1
EnsafCSharpProj <75618286+EnsafCSharpProj@users.noreply.github.com>
2021-02-12 14:56:22 +0100

    s

--- projet_ASP/Controllers/AdministrateurController.cs
+++ projet_ASP/Controllers/AdministrateurController.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using projet_ASP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+
 
 namespace projet_ASP.Controllers
 {
     public class AdministrateurController : Controller
     {
         // GET: Administrateur
-        public ActionResult Index()
+        public ActionResult Propietaires()
         {
-            return View();
+            ApplicationDbContext db = new ApplicationDbContext();
+            var prop = db.Proprietaires.Include(p => p.ApplicationUser).Include(t => t.Voitures).ToList();
+
+            return View(prop);
         }
     }
 }
